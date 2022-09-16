9e4c6da7c47ff473bbda5439de814fd38895c7e4
drgazelle <64329293+drgazelle@users.noreply.github.com>
2022-09-16 09:08:39 -0400

    V0.2

--- src/Course.java
+++ src/Course.java
@@ -4,8 +4,23 @@ import java.util.ArrayList;
  *  of Sets
  *  @author RMizelle
  *  @since 9/16/2022
- *  @version V0.1
+ *  @version V0.2
  */
 public class Course {
     private ArrayList<Set> course = new ArrayList<Set>();
+
+    /** accessor method for sets.
+     * @return Set object at index i; null if out-of-bounds
+     * @param i set index
+     */
+    public Set getSet(int i) {
+        if (i >= course.size()) {
+            return null;
+        }
+        return course.get(i);
+    }
+
+    public void removeSet() {
+
+    }
 }
