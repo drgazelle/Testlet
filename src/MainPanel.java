8e5ea4c905bb79dffa9f0d07250858cb4ec85ede
drgazelle <64329293+drgazelle@users.noreply.github.com>
2022-09-20 09:34:26 -0400

    renamed from TestletPanel >> MainPanel

---
+++ src/MainPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/** MainPanel class defines the containing window
+ *  for the application
+ *  @author ACraig, RMizelle
+ *  @version V0.1
+ */
+public class MainPanel extends JPanel //implements MouseListener, MouseMotionListener
+{
+    //screen size
+   // private static final int xSIZE = 1000; //originally 700
+    //private static final int ySIZE = 600;
+
+}
