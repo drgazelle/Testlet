bb0bef7d27b915d93f076d0ba7867baf4cca173c
drgazelle <64329293+drgazelle@users.noreply.github.com>
2022-09-22 11:05:02 -0400

    improved importData()

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /** Database class creates a tree that
  *  can be imported and exported to a
@@ -8,26 +11,87 @@ import java.util.ArrayList;
  *  <p> Database > Course > Deck > Flashcard </p>
  *
  *  @author RMizelle
- *  @version V0.1
+ *  @version V0.1.1
  */
 public class Database {
 
     private ArrayList<Course> database;
+    private File data;
 
     /** 0-arg constructor implements ArrayList of course objects */
     public Database() {
         //checks for data.txt
-
-        //if data.txt exists, calls import method
-
-        //else instantiates empty Database
+        try {
+            data = new File("resources/data.txt");
+            //if data.txt does not exist, instantiates empty Database
+            if (data.createNewFile()) {
+                System.out.println("New Data File Generated");
+                database = new ArrayList<>();
+            }
+            //else data.txt exists, calls import method
+            else {
+                System.out.println("Accessing Data...");
+                if (importData()) {
+                    System.out.println("Data Retrieved Successfully");
+                }
+                else {
+                    System.out.println("ERROR: Failed to Retrieve Data");
+                }
+            }
+        }
+        catch (IOException e) {
+            //Error when generating Database
+            System.out.println("ERROR: Could not generate Database");
+            e.printStackTrace();
+        }
     }
 
     //////////////////// Data Methods ////////////////////
 
-    /** importData method instantiates database using data.txt */
-    private void importData() {
-        //TO-DO
+    /** importData method instantiates database using data.txt
+     *  @return true if successfully instantiated database, false if error
+     */
+    private boolean importData() {
+        //course index
+        int cIndex = 0;
+        // deck index
+        int dIndex = 0;
+        // flashcard index
+        int fIndex = 0;
+
+        try {
+            //creates scanner
+            Scanner input = new Scanner(data);
+            while (input.hasNextLine()) {
+                String data = input.nextLine();
+                //if data is a course
+                if (data.contains("******")) {
+                    //adds new course
+                    database.add(cIndex, new Course(data.substring(6)));
+                    //resets deck and course index
+                    dIndex = 0;
+                    fIndex = 0;
+                    // increments course index
+                    cIndex++;
+                }
+                //else if data is a deck
+                else if (data.contains("****")) {
+
+                }
+                //else if data is a flashcard
+                else if (data.contains("**")) {
+
+                }
+                else {
+                    System.out.println("ERROR: Reading Data");
+                    return false;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Couldn't Read File");
+            e.printStackTrace();
+        }
+        return true;
     }
 
     /** exportData method takes existing Database and implements data.txt */
