575d7e1312395ab5f41f5520f6ae9e71201ff70f
raisincinnamon <alcswim16@gmail.com>
2022-09-28 09:37:46 -0400

    Added home button

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,22 +1,22 @@
 import javax.swing.*;
 //import java.util.*;
-//import java.awt.*;
+import java.awt.*;
 //import java.awt.event.*;
-//import java.awt.Shape;
+import java.awt.Shape;
 import java.awt.Color;
 //import java.awt.geom.Ellipse2D;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseEvent;
 import java.awt.Graphics;
-//import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;
 //import java.awt.Container;
 //import java.util.ArrayList;
 
 /** MainPanel class defines the containing window
  *  for the application
  *  @author ACraig, RMizelle
- *  @version V0.3
+ *  @version V0.4
  */
 public class MainPanel extends JPanel implements MouseListener, MouseMotionListener
 {
@@ -61,10 +61,10 @@ public class MainPanel extends JPanel implements MouseListener, MouseMotionListe
         //ImageIcon startButton2 = new ImageIcon("images/buttons/start2.png");
         //buttons[STARTBUTTON] = new Button(r1, "start", startButton1, startButton2);
 
-     /*   Shape r1 = new Rectangle(0, 0, 120, 50);
-        ImageIcon homeButton1 = new ImageIcon("images/buttons/home1.png");
-        ImageIcon homeButton2 = new ImageIcon("images/buttons/home2.png");
-        buttons[HOMEBUTTON] = new Button(r1, "home", homeButton1, homeButton2);*/
+        Shape r1 = new Rectangle(0, 0, 120, 50);
+        ImageIcon homeButton1 = new ImageIcon("src/home1.png");
+        ImageIcon homeButton2 = new ImageIcon("src/home1.png"); //same image as homeButton1
+        buttons[HOMEBUTTON] = new Button(r1, "home", homeButton1, homeButton2);
 
         //sets the screen mode
         screenMode = HOMESCREEN;
@@ -78,18 +78,25 @@ public class MainPanel extends JPanel implements MouseListener, MouseMotionListe
             g.setColor(Color.WHITE);
             g.fillRect(0,0, xSIZE, ySIZE);
 
+            //top bar with settings buttons
             g.setColor(new Color(110, 220, 230));
             g.fillRect(120,0,xSIZE-120, 50);
 
             //draw buttons
-            //example below:
-            //buttons[HOMEBUTTON].drawButton(g);
+            buttons[HOMEBUTTON].drawButton(g);
+            buttons[HOMEBUTTON].setEnabled(true);
+            buttons[HOMEBUTTON].setVisible(true);
             repaint();
         }
         else //if(gameMode == )
         {
             g.setColor(new Color(0, 0, 0));
             g.fillRect(0,0,xSIZE, ySIZE);
+
+            //draw buttons
+            buttons[HOMEBUTTON].drawButton(g);
+            buttons[HOMEBUTTON].setEnabled(true);
+            buttons[HOMEBUTTON].setVisible(true);
 
             repaint();
         }
@@ -113,24 +120,26 @@ public class MainPanel extends JPanel implements MouseListener, MouseMotionListe
             }
         }*/
         /*else
-         {
+         { */
          //***BUTTON CODE***actions if clicked on button
             for(Button b:buttons)
             {
                if(b.getShape().contains(mouseX, mouseY) && b.isEnabled())
                {
+                   //examples
                   /* if(b.getTitle().equals("quit"))
                      System.exit(0);
                   else if(b.getTitle().equals("reset"))
                      resetEnemies();
                   else if(b.getTitle().equals("sound"))
                      Sound.click();*/
-         /*
-        if(b.getTitle().equals("start")) {
-            //insert what we need to do when this button is clicked
-        }
-        }
-        }*/
+
+                   if (b.getTitle().equals("home"))  //when the home button is clicked, take the user back to the home screen
+                   {
+                       screenMode = HOMESCREEN;
+                   }
+               }
+              } //end of buttons for loop
     } //end of mouseClicked
 
     public void mousePressed( MouseEvent e )
@@ -148,12 +157,12 @@ public class MainPanel extends JPanel implements MouseListener, MouseMotionListe
         mouseX = e.getX();
         mouseY = e.getY();
         //***BUTTON CODE***highlight button if mouse is on it
-      /*  for (Button b : buttons) {
+        for (Button b : buttons) {
             if (b.getShape().contains(mouseX, mouseY) && b.isEnabled())
                 b.highlight();
             else
-                b.unHighlight();*/
-       // }
+                b.unHighlight();
+        }
     }
 
     public void mouseDragged( MouseEvent e)
